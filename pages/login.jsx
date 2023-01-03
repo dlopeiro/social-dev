e5cf3a55cf10f040ace5f3932382a993c2f1288e
dlopeiro <dribeir4@gmail.com>
2023-01-03 19:52:05 -0300

    basic login form layout created

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,9 +1,39 @@
+import styled from "styled-components"
+
 import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+import H1 from "../src/components/typography/H1"
+import H2 from "../src/components/typography/H2"
+import H4 from "../src/components/typography/H4"
+
+const FormContainer = styled.div`
+  margin-top: 60px;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin: 20px 0;
+  gap: 20px;
+`
+
+const Text = styled.p`
+  text-align: center;
+`
 
 function LoginPage () {
   return (
     <ImageWithSpace>
-      test 123
+      <H1># Social Dev</H1>
+      <H4>All things dev are here</H4>
+      <FormContainer>
+        <H2>Log into your account</H2>
+        <Form>
+          <input placeholder="Email or user" type="email" />
+          <input placeholder="Password" type="password" />
+          <button>Enter</button>
+        </Form>
+        <Text>Are you still not a member? <a href="#">Sign up.</a></Text>
+      </FormContainer>
     </ImageWithSpace>
   )
 }
