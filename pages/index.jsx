1f995e386be34d202100ca7c5e2ec92b8765dc8f
dlopeiro <dribeir4@gmail.com>
2023-01-05 03:12:33 -0300

    post component created

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,32 @@ import styled from "styled-components"
 import Navbar from "../src/components/layout/Navbar"
 import Container from "../src/components/layout/Container"
 import CreatePost from "../src/components/cards/CreatePost"
+import H3 from "../src/components/typography/H3"
+import Post from "../src/components/cards/Post"
 
 const Content = styled.div`
   margin: 50px 0%;
+`
+
+const LastPostText = styled(H3)` // using styled(__) rather than styled.__ makes the StyledComponent between )( adopt the extra styles
+  padding: 40px 0;
+`
+
+const RefreshPosts = styled.span`
+  font-weight: bold;
+  color: ${props => props.theme.primary};
+  cursor: pointer;
+`
+
+const RefreshPostsContainer = styled.div`
+  text-align: center;
+`
+
+const PostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px;
 `
 
 function HomePage () {
@@ -16,6 +39,17 @@ function HomePage () {
       <Content>
         <Container>
           <CreatePost />
+          <LastPostText>
+            <H3>Latest posts:</H3>
+          </LastPostText>
+          <RefreshPostsContainer>
+            <RefreshPosts>Load new posts</RefreshPosts>
+          </RefreshPostsContainer>
+          <PostContainer>
+            <Post />
+            <Post />
+            <Post />
+          </PostContainer>
         </Container>
       </Content>
     </>
