aa1d4dd6c17371320f9d53d08da9351026217a15
dlopeiro <dribeir4@gmail.com>
2023-01-05 02:30:32 -0300

    create post component created

---
+++ src/components/cards/CreatePost.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components"
+
+import H4 from "../typography/H4"
+import Textarea from "../inputs/TextArea"
+import Button from "../inputs/Button"
+
+const PostContainer = styled.div`
+  background-color: ${props => props.theme.white};
+  padding: 20px 40px;
+
+  @media (max-width: 440px) {
+    padding: 20px;
+  }
+`
+
+const Title = styled.div`
+  font-weight: bold;
+  text-align: center;
+`
+
+const TextContainer = styled.div`
+  margin: 20px 0;
+  width: 100%;
+`
+
+const BottomContainer = styled.div`
+  display: flex;  
+  align-items: center;
+  gap: 7px;
+
+  @media (max-width: 440px) {
+    flex-direction: column-reverse;
+  }
+`
+
+const BottomText = styled.p`
+  flex: 1;
+`
+
+function CreatePost () {
+  return (
+    <PostContainer>
+      <H4><Title>What's in your mind, @nick?</Title></H4>
+      <TextContainer>
+        <Textarea placeholder="Type something" rows="4"/>
+      </TextContainer>
+      <BottomContainer>
+        <BottomText>This message will be public</BottomText>
+        <Button>Send message</Button>
+      </BottomContainer>
+    </PostContainer>
+  )
+}
+
+export default CreatePost
