e05d8832afcae5e7f7a6bfd634a6b2c517d27873
dlopeiro <dribeir4@gmail.com>
2023-01-10 20:35:32 -0300

    login route created

--- modules/user/user.service.js
+++ modules/user/user.service.js
@@ -1,4 +1,4 @@
-import { hashPassword } from "../../utils/bcrypt"
+import { hashPassword, comparePassword } from "../../utils/bcrypt"
 
 import User from "./user.module"
 
@@ -10,6 +10,25 @@ export const signupUser = async (body) => {
     }
     const dbUser = await User.create(user)
     return dbUser
+  } catch (err) {
+    throw err
+  }
+}
+
+export const login = async (body) => {
+  try {
+    const user = await User.findOne({
+      $or: [
+        { email: body.userOrEmail },
+        { user: body.userOrEmail }
+      ]
+    })
+
+    if (!user) throw new Error('user not found')
+    const isPasswordCorrect = comparePassword(body.password, user.password)
+    if (!isPasswordCorrect) throw new Error('incorrect password')
+
+  return user    
   } catch (err) {
     throw err
   }
