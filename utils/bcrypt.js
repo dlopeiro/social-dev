513f3910b014a4a4e6eacb1775df404995d9c1c8
dlopeiro <dribeir4@gmail.com>
2023-01-09 23:32:29 -0300

    user db model created

---
+++ utils/bcrypt.js
@@ -0,0 +1,5 @@
+import bcryptjs from "bcryptjs"
+
+export const hashPassword = (password) => bcryptjs.hashSync(password)
+
+export const comparePassword = (password, hash) => bcryptjs.compareSync(password, hash)
