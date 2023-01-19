1f818016630a1705efe4443c52df17ab14f80490
dlopeiro <dribeir4@gmail.com>
2023-01-19 10:38:31 -0300

    textarea components check

--- src/components/inputs/ControllerTextarea.jsx
+++ src/components/inputs/ControllerTextarea.jsx
@@ -1,6 +1,6 @@
 import { useController } from "react-hook-form"
 
-import Textarea from "./Textarea"
+import TextArea from "./Textarea"
 
 
 const ControllerTextarea = ({ name, control, defaultValue = '', ...props }) => {
@@ -9,7 +9,7 @@ const ControllerTextarea = ({ name, control, defaultValue = '', ...props }) => {
   } = useController({ name, control, defaultValue })
 
   return (
-    <Textarea {...props} value={value} onChange={onChange} />
+    <TextArea {...props} value={value} onChange={onChange} />
   )
 }
 
